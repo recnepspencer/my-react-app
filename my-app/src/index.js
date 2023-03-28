48a0604e13495eb7042817097a5d7a944c9e9c83
recnepspencer <goldenspencerh@gmail.com>
2023-03-28 15:34:47 -0600

    created a notes component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notes from './notes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Notes />
   </React.StrictMode>
 );
 
---
+++ my-app/src/notes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class Notes extends Component {
+    render(){
+        return (
+            <div>
+                <h1>These are my notes</h1>
+            </div>
+        )
+    }
+}
+export default Notes
